27238bf8d589cdc441a6bc04ac7f5aee4c0f0827
wafiqrazy035-art <wafiqrazy035@gmail.com>
2026-05-10 12:14:02 +0700

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,9 +1,10 @@
 import requests
+import os
 
 class DiscordNotifier:
     def __init__(self):
         # GANTI URL DI BAWAH dengan Webhook URL dari Discord kamu
-        self.webhook_url = "https://discord.com/api/webhooks/1455436201644527657/jYnpBlepwwcOhTGrmDWeaKNf9yZI7Q7FQPQPsMsz1nhqVDCKC7O5SVReZiEBa-5ACKfn"
+        self.webhook_url = os.getenv("DISCORD_WEBHOOK_URL")
 
     def send_notification(self, pesan):
         data = {
